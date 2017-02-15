8ffbcca7a6dab46b3aeb97b20e874b01898e9b2c
tsahibspele <tsahibspele@gmail.com>
2017-02-15 13:17:11 +0200

    More styling to the app

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,9 +4,7 @@ var React = require('react');
 var WeatherMessage = ({temp, location}) => {
 
 	return (
-		<div>
-			<h4>The temperture in {location} is {temp}</h4>
-		</div>
+		<h3 className="text-center">The temperture in {location} is {temp}</h3>
 	);
 }
 
